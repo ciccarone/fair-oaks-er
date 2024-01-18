cbec9f4c588965b1813aa8021564e9f76f7ff119
Tony Ciccarone <tonyvciccarone@gmail.com>
2024-01-18 09:26:43 -0500

    finish page

--- public/wp-content/themes/fairoakser/script.js
+++ public/wp-content/themes/fairoakser/script.js
@@ -2,11 +2,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const acc_holder = document.querySelector('.acc__holder');
     const acc = document.querySelectorAll('.acc');
 
+    console.log(`Found ${acc.length} .acc elements`);
+
     acc.forEach((element, index) => {
         element.classList.add('disabled');
         if(index === 0) {
             element.classList.add('active');
             element.classList.remove('disabled');
         }
+    });
+
+    const acc_headers = document.querySelectorAll('.acc__header');
+    console.log(`Found ${acc_headers.length} .acc__header elements`);
+
+    acc_headers.forEach((header) => {
+        header.addEventListener('click', (event) => {
+            console.log('Header clicked');
+            acc.forEach((element) => {
+                element.classList.remove('active');
+                element.classList.add('disabled');
+            });
+            const parentAcc = header.parentElement;
+            parentAcc.classList.add('active');
+            parentAcc.classList.remove('disabled');
+        });
     });
 });
