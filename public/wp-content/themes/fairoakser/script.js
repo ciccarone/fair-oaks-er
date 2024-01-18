f00099ec4bbdf011ec8759d5bd28e6051beedb04
Tony Ciccarone <tonyvciccarone@gmail.com>
2024-01-18 08:48:53 -0500

    test auto

--- public/wp-content/themes/fairoakser/script.js
+++ public/wp-content/themes/fairoakser/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+    const acc_holder = document.querySelector('.acc__holder');
     const acc = document.querySelector('.acc');
-    alert(acc);
  
 });
