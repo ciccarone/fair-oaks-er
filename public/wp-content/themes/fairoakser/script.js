eb0182510827649bf902366f9ff51b28fd430f83
Tony Ciccarone <tonyvciccarone@gmail.com>
2024-01-18 08:56:59 -0500

    add active/disabled classes to accordion

--- public/wp-content/themes/fairoakser/script.js
+++ public/wp-content/themes/fairoakser/script.js
@@ -1,6 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-
     const acc_holder = document.querySelector('.acc__holder');
-    const acc = document.querySelector('.acc');
- 
+    const acc = document.querySelectorAll('.acc');
+
+    acc.forEach((element, index) => {
+        element.classList.add('disabled');
+        if(index === 0) {
+            element.classList.add('active');
+            element.classList.remove('disabled');
+        }
+    });
 });
